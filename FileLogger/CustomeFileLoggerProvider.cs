0f8eba75bfa03c30bc94ec65a77a59dd5adefcc4
NSwimer1321 <njtaylor2021@gmail.com>
2023-03-29 12:37:14 -0600

    GUI completed (Still need to add functionality). Readded FileLogger project.

---
+++ FileLogger/CustomeFileLoggerProvider.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileLogger
+{
+    public class CustomeFileLoggerProvider : ILoggerProvider
+    {
+        public ILogger CreateLogger(string categoryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
