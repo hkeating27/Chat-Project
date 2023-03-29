24a12f6c8c34fc5410bf3b60ba7c74fa7e44c23b
NSwimer1321 <njtaylor2021@gmail.com>
2023-03-29 13:14:53 -0600

    Added some code to Connect. Fixed some errors in constructer.

--- Networking_and_Logging/Communications/Networking.cs
+++ Networking_and_Logging/Communications/Networking.cs
@@ -4,18 +4,54 @@ namespace Communications
 {
     public class Networking
     {
+        public delegate void ReportMessageArrived(Networking channel, string message);
+        public delegate void ReportDisconnect(Networking channel);
+        public delegate void ReportConnectionEstablished(Networking channel);
+
         //Fields
-        private delegate void ReportMessageArrived(Networking channel, string message);
-        private delegate void ReportDisconnect(Networking channel);
-        private delegate void ReportConnectionEstablished(Networking channel);
-        
+        private ReportMessageArrived onMessage;
+        private ReportDisconnect reportDisconnect;
+        private ReportConnectionEstablished onConnect;
+
         public string ID { get; set; }
 
-        public Networking(ILogger logger, ReportConnectionEstablished onConnect, ReportDisconnect reportDisconnect
+        public Networking(ILogger logger, ReportConnectionEstablished onConnect, ReportDisconnect reportDisconnect,
                           ReportMessageArrived onMessage, char terminationCharacter)
+        {
+            ID = "Jim";
+            this.onMessage = onMessage;
+            this.reportDisconnect = reportDisconnect;
+            this.onConnect = onConnect;
+        }
+
+        public void Connect(string host, int port)
+        {
+            
+        }
+
+        public async void AwaitMessagesAsync(bool infinite = true)
         {
 
         }
 
+        public async void WaitForClients(int port, bool infinte)
+        {
+
+        }
+
+        public void StopWaitingForClients()
+        {
+
+        }
+
+        public void Disconnect()
+        {
+
+        }
+
+        public async void Start(string text)
+        {
+
+        }
     }
 }
